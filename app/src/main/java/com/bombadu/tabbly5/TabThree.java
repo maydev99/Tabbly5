f9e9f8b3c8c0fe31dcb70ff49b0be5f1ce2d4e02
michael <maydev99@gmail.com>
2019-12-26 16:10:09 -0500

    Change Text Color with Tap

--- app/src/main/java/com/bombadu/tabbly5/TabThree.java
+++ app/src/main/java/com/bombadu/tabbly5/TabThree.java
@@ -1,19 +1,36 @@
 package com.bombadu.tabbly5;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class TabThree extends Fragment {
+    boolean isTextColor = false;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab3, container, false);
+
+        final TextView threeTextView = view.findViewById(R.id.threeTextView);
+        threeTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isTextColor){
+                    threeTextView.setTextColor(Color.BLUE);
+                    isTextColor = true;
+                } else {
+                    threeTextView.setTextColor(Color.BLACK);
+                    isTextColor = false;
+                }
+            }
+        });
         return view;
     }
 }
--- app/src/main/java/com/bombadu/tabbly5/TabTwo.java
+++ app/src/main/java/com/bombadu/tabbly5/TabTwo.java
@@ -1,19 +1,37 @@
 package com.bombadu.tabbly5;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class TabTwo extends Fragment {
+    boolean isTextColor = false;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab2, container, false);
+
+        final TextView twoTextView = view.findViewById(R.id.twoTextView);
+        twoTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isTextColor){
+                    twoTextView.setTextColor(Color.GREEN);
+
+                    isTextColor = true;
+                } else {
+                    twoTextView.setTextColor(Color.BLACK);
+                    isTextColor = false;
+                }
+            }
+        });
         return view;
     }
 }
